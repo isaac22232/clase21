6648523fc2400a2782350a95703467bfda5b2083
isaac22232 <102384052+isaac22232@users.noreply.github.com>
2022-06-10 12:49:57 -0500

    Add files via upload

---
+++ PROC21-AA-main/PROC21-AA-main/sketch.js
@@ -0,0 +1,62 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+let engine;
+let world;
+
+var ground;
+var left;
+var right;
+var top_wall;
+var ball;
+var flecha1,flecha2;
+
+function setup() {
+  createCanvas(400,400);
+  engine = Engine.create();
+  world = engine.world;
+  
+  ground =new Ground(200,390,400,20);
+  right = new Ground(390,200,20,400);
+  left = new Ground(10,200,20,400);
+  top_wall = new Ground(200,10,400,20);
+ 
+  var options = {
+    restitution:0.95
+  }
+
+  ball=Bodies.circle(200,100,20,options)
+World.add(world,ball)
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+flecha1=createImg("right.png")
+flecha1.position(220,30)
+flecha1.size(50,50)
+flecha1.mouseClicked(fh)
+flecha2=createImg("up.png")
+flecha2.position(20,30)
+flecha2.size(50,50)
+flecha2.mouseClicked(fv)
+}
+
+function draw() 
+{
+  background(51);
+  ground.show();
+  top_wall.show();
+  left.show();
+  right.show();
+  Engine.update(engine);
+  
+  ellipse(ball.position.x,ball.position.y,20)
+}
+
+function fh(){
+  Matter.Body.applyForce(ball,{x:0,y:0},{x:0.05,y:0})
+}
+
+function fv(){
+  Matter.Body.applyForce(ball,{x:0,y:0},{x:0,y:-0.05})
+}
